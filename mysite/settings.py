8d9b695f957623c3852b4997a21ca4655c96d4ee
Anastasiia Derevianko <anastasiia1010@gmail.com>
2020-10-23 19:08:08 +0300

    add ability to run code from docker (#6)

--- mysite/settings.py
+++ mysite/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = ')pqx*7)0psbgm@mq=!6winbbo3tkz1r)0il-gijzhv1fyx--p5'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['0.0.0.0']
 
 
 # Application definition
